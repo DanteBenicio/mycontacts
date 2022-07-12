3a067edc21200bafce1aabf25f182deed27c3120
Dante Benicio <dantecosta79@gmail.com>
2022-07-12 20:31:39 -0300

    application middlewares

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello Worlded\n');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('Server is running at http://localhost:3000');
