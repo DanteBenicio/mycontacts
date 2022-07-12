1833582978300b1bde8889180ad62a5e12789cc4
Dante Benicio <dantecosta79@gmail.com>
2022-07-12 20:31:58 -0300

    changed port value

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Server is running at http://localhost:3000');
 });
