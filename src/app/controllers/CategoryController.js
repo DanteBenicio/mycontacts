26eb1320f906b33c864cc1fa16dc7f821fbabfce
Dante Benicio <dantecosta79@gmail.com>
2022-07-13 20:45:06 -0300

    create CategoryController

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,37 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(400).json({ error: 'category does not exist' });
+    }
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
