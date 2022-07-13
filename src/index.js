2da47a98741784ed519d259f2468a5cb11b715ab
Dante Benicio <dantecosta79@gmail.com>
2022-07-13 20:47:28 -0300

    express error handler

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+//! Error Handler -> Always come after routes and always is necessary have four params
+app.use((error, request, response, next) => {
+  console.log('-> Error Handler <-');
+  console.log(error);
+  response.sendStatus(500);
+});
+
 app.listen(3001, () => {
   console.log('Server is running at http://localhost:3000');
 });
